c7521d23266283fa395519f385f0c41fb7617018
Evan Lackey <elackey@sandiego.edu>
2025-09-15 10:29:51 -0700

    feat: Created strings.js to test string functions

---
+++ strings.js
@@ -0,0 +1,46 @@
+/**
+ * String utilities for learning purposes
+ * @file 'strings.js'
+ * @author 'Evan Lackey'
+ */
+
+/**
+ * Reverse a string
+ * @param {string} str - string to revers
+ * @returns {string} Reversed string
+ */
+
+export function revers(str) {
+    return 'stubbed';
+}
+
+/**
+ * Capitalize the first letter of a string
+ * @param {string} str - String to capitalize
+ * @returns {string} Capitalized string
+ */
+
+export function capitalize(str) {
+    return 'stubbed';
+}
+
+/**
+ * count vowels in a string
+ * @param {string} str - String to count vowels
+ * @returns {number} Number of vowels
+ */
+
+export function countVowels(str) {
+    return 'stubbed';
+}
+
+/**
+ * check if string is palindrome
+ * @param {string} str - String to check
+ * @returns {boolean} True if palindrome
+ */
+
+export function isPalindrome(str) {
+    return 'stubbed';
+}
+
