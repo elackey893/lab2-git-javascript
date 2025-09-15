254f320ff6984efb66fbf32e5992858a224819d6
Evan Lackey <elackey@sandiego.edu>
2025-09-14 20:47:41 -0700

    feat: Created index.js to test math module

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * Main entry point for Lab 2
+ * @file 'index.js'
+ * @author 'Evan Lackey'
+ */
+
+// Import math functions
+import * as math from './math.js';
+
+console.log('=== Testing Math Module ===');
+console.log('add(5,3) = ', math.add(5,3));
+console.log('subtract(10,4)=',math.subtract(10,4));
+console.log('multiply(3,7) =',math.multiply(3,7));
+console.log('divide(20,4) =', math.divide(20,4));
+// console.log('divide(3,0) =',math.divide(3,0));
+console.log('power(2,8) =', math.power(2,8));
+console.log('sqrt(16) =',math.sqrt(16));
+console.log('factorial(5) =', math.factorial(5));
